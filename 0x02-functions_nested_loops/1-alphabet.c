5be887c465602395e070ce86abc281d67d761229
root <csayasmamaw23@gmail.com>
2021-06-22 07:07:04 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 /**
  * alphabet using _putchar
+ * Discription students shouldnt us standard input output 
  */
 
 void print_alphabet(void)
 {
-
 	int alphabet;
-
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for( alphabet = 'a'; alphabet <= 'z'; alphabet++ )
 	{
              _putchar(alphabet);
 	}
