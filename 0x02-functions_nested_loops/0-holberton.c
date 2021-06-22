e27b200da47f0e4fdbd76fe30d64880e11a5b8bc
root <csayasmamaw23@gmail.com>
2021-06-22 05:20:29 -0700

    function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+   putchar('H');
+   putchar('o');
+   putchar('l');
+   putchar('b');
+   putchar('e');
+   putchar('r');
+   putchar('t');
+   putchar('o');
+   putchar('n');
+
+  putchar('\n');   
+   return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is function nusted loop readme
