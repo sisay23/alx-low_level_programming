0bebe598d92f478dd17eaeeb8ba3a4e1fe6c9cf8
root <csayasmamaw23@gmail.com>
2021-06-22 06:43:12 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * alphabet using _putchar
+ */
+
+void print_alphabet(void){
+
+	int alphabet;
+
+	for(alphabet='a';alphabet<='z';alphabet++){
+             _putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
