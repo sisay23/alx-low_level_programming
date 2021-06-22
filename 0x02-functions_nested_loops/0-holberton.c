d04fc0d36d3c1632f6a2ec9009ce7b47c541ee35
root <csayasmamaw23@gmail.com>
2021-06-22 06:21:23 -0700

    C function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
-int main(){
-   putchar('H');
-   putchar('o');
-   putchar('l');
-   putchar('b');
-   putchar('e');
-   putchar('r');
-   putchar('t');
-   putchar('o');
-   putchar('n');
+#include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char holberton[] = "Holberton";
+	int i = 0;
 
-  putchar('\n');   
-   return 0;
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
