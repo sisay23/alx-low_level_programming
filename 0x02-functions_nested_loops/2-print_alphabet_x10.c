093c48f5f4449f343e72cd9eaec35b18236839db
root <csayasmamaw23@gmail.com>
2021-06-22 08:52:29 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int alphabet;
 	for (count  = 0; count < 10; count++)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			
 			_putchar(alphabet);
