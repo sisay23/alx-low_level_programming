b18c50fa8b0f8030c8ded2281db6d92ec62827be
root <csayasmamaw23@gmail.com>
2021-06-22 07:13:32 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,12 @@
 
 void print_alphabet(void)
 {
+
 	int alphabet;
-	for( alphabet = 'a'; alphabet <= 'z'; alphabet++ )
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
+
              _putchar(alphabet);
 	}
 
