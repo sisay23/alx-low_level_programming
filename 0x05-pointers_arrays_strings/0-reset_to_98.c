4a894cad28cf0e05239826ff69182f5aaa35b488
root <csayasmamaw23@gmail.com>
2021-06-28 07:53:45 -0700

    array and pointer commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - change the value of anumber into 98
+ * @n: pointer to the number
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is arry and pointer
