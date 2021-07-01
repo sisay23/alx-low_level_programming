a07a97801acee10cdfa2d4d3b0721064d6f0bc92
root <csayasmamaw23@gmail.com>
2021-07-01 09:57:10 -0700

    more pointer,array and string

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main_function - magic happen
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
